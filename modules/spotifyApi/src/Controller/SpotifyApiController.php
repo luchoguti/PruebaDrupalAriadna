57fea922fb9db0248caac0a843641f8e077d2928
luis.torres <luchoguti9@gmail.com>
2020-11-13 23:03:48 -0500

    The files necessary for the creation of the api spotify module are implemented

---
+++ modules/spotifyApi/src/Controller/SpotifyApiController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Drupal\spotifyApi\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use GuzzleHttp\Exception\GuzzleException;
+
+class SpotifyApiController extends ControllerBase
+{
+
+}
